15d5c0836b64b7f48906d721881bfc8935cc315b
karkiamrit <amrit.karki2073@gmail.com>
2023-05-25 18:40:54 +0545

    use alternative to bcrypt due to comparison issue

--- controller/userController.js
+++ controller/userController.js
@@ -1,175 +1,139 @@
 const User = require('../model/userModel');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
 
-//Generate hashed Password
-const hashPassword = async(password, saltRounds, callback) => {
-    // Generate a random salt
-    const salt = crypto.randomBytes(16).toString('hex');
-  
-    // Hash the password with SHA256
-    const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
-  
-    // Hash the SHA256 password with bcrypt and the salt
-    bcrypt.hash(hashedPassword, saltRounds, (err, hash) => {
-      if (err) {
-        return callback(err);
-      }
-  
-      callback(hash, salt);
-    });
-  };
-  
-  // Create a user 
-  exports.createUser = async(req, res, next) => {
-    const { id, username, email, password, name, role, phone, address, createdAt, updatedAt } = req.body;
-  
-    await hashPassword(password, 10, (hashedPassword, salt) => {
-      const user = {
-        id,
-        username,
-        email,
-        password: hashedPassword,
-        name,
-        phone,
-        role,
-        address,
-        createdAt,
-        updatedAt
-      };
-  
-      User.getByColumnValue('username', username, (error, usernameInfo) => {
-        if (error) {
-          console.error('Error checking username:', error);
-          res.status(500).json({ success: false, error: 'Failed to check username' });
-          return;
-        }
-  
-        if (usernameInfo) {
-          console.log('User with the same username already exists');
-          res.status(201).json({ success: false, message: 'User with the same username already exists' });
-          return;
-        }
-  
-        User.getByColumnValue('email', email, (error, emailInfo) => {
-          if (error) {
-            console.error('Error checking email:', error);
-            res.status(500).json({ success: false, error: 'Failed to check email' });
-            return;
-          }
-  
-          if (emailInfo) {
-            console.log('User with the same email already exists');
-            res.status(201).json({ success: false, error: 'User with the same email already exists' });
-            return;
-          }
-  
-          User.create(user, (error, createdUser) => {
-            if (error) {
-              console.error('Error creating user:', error);
-              res.status(500).json({ success: false, message: 'Failed to create user' });
-              return;
-            }
-  
-            res.status(201).json({ success: true, user: createdUser });
-          });
-        });
-      });
-    });
-  };
-  
 
-exports.getAllUsers = (req, res, next) => {
-    User.getAll((error, UsersInfo) => {
-        if (error) {
-            console.error('Error Checking Users Info', error);
-            res.status(500).json({ success: false, error: 'Failed to retrieve user details' });
-            return;
-        }
-        res.status(201).json({ success: true, user: UsersInfo });
-    })
-}
+// Generate hashed Password
+const hashPassword = (password, salt) => {
+  const hashedPassword = crypto
+    .createHmac('sha256', salt)
+    .update(password)
+    .digest('hex'); // Hash the password with the salt
 
-exports.getSingleUser = (req, res, next) => {
-    const id = req.params.id;
-    User.getById(id,(error, UserInfo) => {
-        if (error) {
-            console.error('Error Checking User Info', error);
-            res.status(500).json({ success: false, error: 'Failed to retrieve user detail' });
-            return;
-        }
-        res.status(201).json({ success: true, user: UserInfo });
-    })
-}
-exports.updateUser = (req, res, next) => {
-    const id = req.params.id;
-    const updatedData = req.body;
-  
-    User.update(id, updatedData, (error, success) => {
-      if (error) {
-        console.error('Error updating user', error);
-        res.status(500).json({ success: false, error: 'Failed to update user detail' });
-        return;
-      }
-      if (!success) {
-        res.status(404).json({ success: false, error: 'User not found' });
-        return;
-      }
-      res.status(200).json({ success: true, message: 'User updated successfully' });
-    });
-  };
-  
-exports.deleteUser = (req, res, next) => {
-   const id =req.params.id;
-   User.delete(id,(error,success)=>{
-    if (error) {
-        console.error('Error deleting user', error);
-        res.status(500).json({ success: false, error: 'Failed to delete user detail' });
-        return;
-    }
-    if (!success) {
-        res.status(404).json({ success: false, error: 'User not found' });
-        return;
-    }
-    res.status(201).json({ success: true, message: 'User deleted Successfully' });
-   });
-
+  return hashedPassword;
 };
 
-exports.loginUser = async(req, res, next) => {
+
+// Create a user
+exports.createUser = async (req, res, next) => {
+  const { id, username, email, password, name, role, phone, address, createdAt, updatedAt } = req.body;
+
+  try {
+    const salt = crypto.randomBytes(16).toString('hex'); // Generate a random salt
+    const hashedPassword = hashPassword(password, salt); // Hash the password with the salt
+
+    const user = {
+      id,
+      username,
+      email,
+      password: `${salt}:${hashedPassword}`, // Combine salt and hashed password
+      name,
+      phone,
+      role,
+      address,
+      createdAt,
+      updatedAt,
+    };
+
+    const usernameInfo = await User.getByColumnValue('username', username);
+    if (usernameInfo) {
+      console.log('User with the same username already exists');
+      return res.status(201).json({ success: false, message: 'User with the same username already exists' });
+    }
+
+    const emailInfo = await User.getByColumnValue('email', email);
+    if (emailInfo) {
+      console.log('User with the same email already exists');
+      return res.status(201).json({ success: false, error: 'User with the same email already exists' });
+    }
+
+    const createdUser = await User.create(user);
+    res.status(201).json({ success: true, user: createdUser });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ success: false, message: 'Failed to create user' });
+  }
+};
+
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const UsersInfo = await User.getAll();
+    res.status(201).json({ success: true, user: UsersInfo });
+  } catch (error) {
+    console.error('Error Checking Users Info', error);
+    res.status(500).json({ success: false, error: 'Failed to retrieve user details' });
+  }
+};
+
+exports.getSingleUser = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const UserInfo = await User.getById(id);
+    res.status(201).json({ success: true, user: UserInfo });
+  } catch (error) {
+    console.error('Error Checking User Info', error);
+    res.status(500).json({ success: false, error: 'Failed to retrieve user detail' });
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+
+  try {
+    const success = await User.update(id, updatedData);
+    if (!success) {
+      return res.status(404).json({ success: false, error: 'User not found' });
+    }
+    res.status(200).json({ success: true, message: 'User updated successfully' });
+  } catch (error) {
+    console.error('Error updating user', error);
+    res.status(500).json({ success: false, error: 'Failed to update user detail' });
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const success = await User.delete(id);
+    if (!success) {
+      return res.status(404).json({ success: false, error: 'User not found' });
+    }
+    res.status(201).json({ success: true, message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting user', error);
+    res.status(500).json({ success: false, error: 'Failed to delete user detail' });
+  }
+};
+
+exports.loginUser = async (req, res, next) => {
   const email = req.body.email;
   const enteredPassword = req.body.password;
 
-  User.getByColumnValue('email', email, (error, userInfo) => {
-    if (error) {
-      console.error('Error checking email:', error);
-      res.status(500).json({ success: false, error: 'Failed to check email' });
-      return;
-    }
+  try {
+    const userInfo = await User.getByColumnValue('email', email);
 
     if (!userInfo) {
       console.error('User not found');
       res.status(401).json({ success: false, message: 'Please enter a valid email and password' });
       return;
     }
-      
 
-    // Compare entered password with stored hashed password
-    bcrypt.compare(enteredPassword, userInfo.password, (err, isMatch) => {
-      if (err) {
-        console.error('Error comparing passwords', err);
-        res.status(500).json({ success: false, error: 'Failed to compare passwords' });
-        return;
-      }
+    const storedPassword = userInfo.password; // Get the stored password from the user object
+    const [salt, hashedPassword] = storedPassword.split(':'); // Split the stored password to extract the salt and hashed password
 
-      
-      if (isMatch) {
-        console.log('Login Success');
-        res.status(200).json({ success: true, message: 'Login Successful' });
-      } else {
-        console.error('Wrong Password');
-        res.status(401).json({ success: false, message: 'Please enter a valid email and password' });
-      }
-    });
-  });
+    const isMatch = hashPassword(enteredPassword, salt) === hashedPassword;
+
+    if (isMatch) {
+      console.log('Login Success');
+      res.status(200).json({ success: true, message: 'Login Successful' });
+    } else {
+      console.error('Wrong Password');
+      res.status(401).json({ success: false, message: 'Please enter a valid email and password' });
+    }
+  } catch (error) {
+    console.error('Error checking email:', error);
+    res.status(500).json({ success: false, error: 'Failed to check email' });
+  }
 };
--- model/userModel.js
+++ model/userModel.js
@@ -1,151 +1,108 @@
 const { getConnection } = require('../config/database');
-
+const { promisify } = require('util');
 
 const User = {
-//Get all users
-  getAll(callback) {
+  // Get all users
+  getAll: async () => {
     const query = 'SELECT * FROM users';
+    const connection = await promisify(getConnection)();
 
-    getConnection((error, connection) => {
-      if (error) {
-        return callback(error, null);
-      }
-
-      connection.query(query, (error, results, fields) => {
-        connection.release();
-
-        if (error) {
-          return callback(error, null);
-        }
-
-        return callback(null, results);
-      });
-    });
+    try {
+      const results = await promisify(connection.query).call(connection, query);
+      return results;
+    } finally {
+      connection.release();
+    }
   },
-//Get user by id
-  getById(id, callback) {
+
+  // Get user by id
+  getById: async (id) => {
     const query = 'SELECT * FROM users WHERE id = ?';
     const values = [id];
+    const connection = await promisify(getConnection)();
 
-    getConnection((error, connection) => {
-      if (error) {
-        return callback(error, null);
+    try {
+      const results = await promisify(connection.query).call(connection, query, values);
+
+      if (results.length === 0) {
+        return null; // User not found
       }
 
-      connection.query(query, values, (error, results, fields) => {
-        connection.release();
-
-        if (error) {
-          return callback(error, null);
-        }
-
-        if (results.length === 0) {
-          return callback(null, null); // User not found
-        }
-
-        return callback(null, results[0]);
-      });
-    });
+      return results[0];
+    } finally {
+      connection.release();
+    }
   },
 
-//get User by column
-  getByColumnValue(column, value, callback) {
+  // Get user by column
+  getByColumnValue: async (column, value) => {
     const query = 'SELECT * FROM users WHERE ?? = ?';
     const formattedQuery = [column, value];
+    const connection = await promisify(getConnection)();
 
-    getConnection((error, connection) => {
-      if (error) {
-        return callback(error, null);
+    try {
+      const results = await promisify(connection.query).call(connection, query, formattedQuery);
+
+      if (results.length === 0) {
+        return null; // User not found
       }
 
-      connection.query(query, formattedQuery, (error, results, fields) => {
-        connection.release();
-
-        if (error) {
-          return callback(error, null);
-        }
-
-        if (results.length === 0) {
-          return callback(null, null); // User not found
-        }
-
-        return callback(null, results[0]);
-      });
-    });
-},
-
-//Insert new user into database
-  create(user, callback) {
-    const query = 'INSERT INTO users SET ?';
-
-    getConnection((error, connection) => {
-      if (error) {
-        return callback(error, null);
-      }
-
-      connection.query(query, user, (error, results, fields) => {
-        connection.release();
-
-        if (error) {
-          return callback(error, null);
-        }
-
-        const createdUser = { id: results.insertId, ...user };
-        return callback(null, createdUser);
-      });
-    });
+      return results[0];
+    } finally {
+      connection.release();
+    }
   },
 
-//Update user info
-  update(id, user, callback) {
+  // Insert new user into database
+  create: async (user) => {
+    const query = 'INSERT INTO users SET ?';
+    const connection = await promisify(getConnection)();
+
+    try {
+      const results = await promisify(connection.query).call(connection, query, user);
+      const createdUser = { id: results.insertId, ...user };
+      return createdUser;
+    } finally {
+      connection.release();
+    }
+  },
+
+  // Update user info
+  update: async (id, user) => {
     const query = 'UPDATE users SET ? WHERE id = ?';
     const values = [user, id];
+    const connection = await promisify(getConnection)();
 
-    getConnection((error, connection) => {
-      if (error) {
-        return callback(error, null);
+    try {
+      const results = await promisify(connection.query).call(connection, query, values);
+
+      if (results.affectedRows === 0) {
+        return false; // User not found
       }
 
-      connection.query(query, values, (error, results, fields) => {
-        connection.release();
-
-        if (error) {
-          return callback(error, null);
-        }
-
-        if (results.affectedRows === 0) {
-          return callback(null, false); // User not found
-        }
-
-        return callback(null, true);
-      });
-    });
+      return true;
+    } finally {
+      connection.release();
+    }
   },
 
-//Delete existing user  
-  delete(id, callback) {
+  // Delete existing user
+  delete: async (id) => {
     const query = 'DELETE FROM users WHERE id = ?';
     const values = [id];
+    const connection = await promisify(getConnection)();
 
-    getConnection((error, connection) => {
-      if (error) {
-        return callback(error, null);
+    try {
+      const results = await promisify(connection.query).call(connection, query, values);
+
+      if (results.affectedRows === 0) {
+        return false; // User not found
       }
 
-      connection.query(query, values, (error, results, fields) => {
-        connection.release();
-
-        if (error) {
-          return callback(error, null);
-        }
-
-        if (results.affectedRows === 0) {
-          return callback(null, false); // User not found
-        }
-
-        return callback(null, true);
-      });
-    });
+      return true;
+    } finally {
+      connection.release();
+    }
   },
 };
 
